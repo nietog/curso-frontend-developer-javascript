0143a17fde92a8791a39e853f5e9d1636ade2454
nietog <camilox83@gmail.com>
2023-03-25 16:22:02 -0500

    main menu and desktop menu incorporated

---
+++ main.js
@@ -0,0 +1,7 @@
+let menuEmail = document.querySelector(".navbar-email");
+let desktopMenu = document.querySelector(".desktop-menu");
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle("inactive");
+}
