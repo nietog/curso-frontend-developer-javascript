e9780038463e743e90d8e2ac9dbbcc93051dca40
nietog <camilox83@gmail.com>
2023-03-25 19:20:11 -0500

    shopping cart incorporated

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const hamIcon = document.querySelector(".hamIcon"); 
 const mobileMenu = document.querySelector(".mobile-menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 hamIcon.addEventListener("click", toggleMobileMenu);
+shoppingCart.addEventListener("click", toggleAside)
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
 }
 
 function toggleMobileMenu () {
     mobileMenu.classList.toggle("inactive");
+    aside.classList.add("inactive");
+}
+
+function toggleAside () {
+    aside.classList.toggle("inactive");
+    mobileMenu.classList.add("inactive");
+    desktopMenu.classList.add("inactive");
 }
