dfc469696464869463b164fddcac3b5b1010b40a
nietog <camilox83@gmail.com>
2023-03-26 00:20:37 -0500

    product list incorporated

--- main.js
+++ main.js
@@ -24,3 +24,67 @@ function toggleAside () {
     mobileMenu.classList.add("inactive");
     desktopMenu.classList.add("inactive");
 }
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: "120,00",
+    image:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+
+productList.push({
+    name: "Computer",
+    price: "300,00",
+    image:"https://images.pexels.com/photos/434346/pexels-photo-434346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+productList.push({
+    name: "Monitor",
+    price: "110,00",
+    image:"https://images.pexels.com/photos/326501/pexels-photo-326501.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+productList.push({
+    name: "Headphones",
+    price: "80,00",
+    image:"https://images.pexels.com/photos/3394665/pexels-photo-3394665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+});
+
+
+function renderProducts (arr) {
+    for (const product of arr) {
+        const container = document.querySelector(".cards-container");
+    
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const image = document.createElement("img");
+        image.src = product.image;
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productDiv = document.createElement("div");
+    
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$"+ product.price;
+    
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        const figure = document.createElement("figure");
+    
+        const addImage = document.createElement("img");
+        addImage.src = "./icons/bt_add_to_cart.svg"
+    
+        figure.appendChild(addImage);
+        productDiv.append(productPrice, productName);
+        productInfo.append(productDiv, figure);
+        productCard.append(image, productInfo);
+        container.appendChild( productCard);
+    }
+}
+
+renderProducts(productList);
+
+
